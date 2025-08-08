d72136be14015d902eae50d4a89a3564506fe756
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-08 01:37:11 -0300

    Serviço para criar uma nova Expense com FluentValidator

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+    {
+        public RegisterExpenseValidator()
+        {
+            RuleFor(expense => expense.Title)
+                .NotEmpty().WithMessage("Title is required.");
+            RuleFor(expense => expense.Date)
+                .LessThanOrEqualTo(DateTime.Now).WithMessage("Date is invalid.");
+            RuleFor(expense => expense.Amount)
+                .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+            RuleFor(expense => expense.PaymentType)
+                .IsInEnum().WithMessage("Invalid payment type.");
+        }
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpensesUseCase.cs
@@ -0,0 +1,34 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpensesUseCase
+    {
+        public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            Validate(request);
+
+            return new ResponseRegisterExpenseJson();
+        }
+
+        #region Private Methods
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var validator = new RegisterExpenseValidator();
+
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+
+        #endregion
+    }
+}
