5856620c07660b97c7a3bf8a2822f90fa64b24de
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-09 23:06:54 -0300

    Implementação de testes unitários

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpensesValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
