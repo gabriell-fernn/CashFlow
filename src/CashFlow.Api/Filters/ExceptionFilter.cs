77fd1bc6b49ecb7ed89f8811c8483ceb182c89a1
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-13 15:48:14 -0300

    Implementação NotFound Exception

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -22,14 +22,19 @@ namespace CashFlow.Api.Filters
 
         private void HandleProjectException(ExceptionContext context)
         {
-            if (context.Exception is ErrorOnValidationException)
+            if (context.Exception is ErrorOnValidationException errorOnValidationException)
             {
-                var ex = (ErrorOnValidationException)context.Exception;
-
-                var errorResponse = new ResponseErrorJson(ex.Errors);
+                var errorResponse = new ResponseErrorJson(errorOnValidationException.Errors);
 
                 context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
                 context.Result = new BadRequestObjectResult(errorResponse);
+            }
+            else if (context.Exception is ExpenseNotFoundException expenseNotFound)
+            {
+                var errorResponse = new ResponseErrorJson(expenseNotFound.Message);
+
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new NotFoundObjectResult(errorResponse);
             }
             else
             {
