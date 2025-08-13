8584edc4f4b932907e563b7ec91c76460208981b
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-13 15:48:51 -0300

    Criação do serviço GetById

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.AutoMapper;
 using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +23,7 @@ namespace CashFlow.Application
         {
             service.AddScoped<IRegisterExpensesUseCase, RegisterExpensesUseCase>();
             service.AddScoped<IGetAllExpensesUseCase, GetAllExpensesUseCase>();
+            service.AddScoped<IGetByIdExpenseUseCase, GetByIdExpenseUseCase>();
         }
     }
 }
