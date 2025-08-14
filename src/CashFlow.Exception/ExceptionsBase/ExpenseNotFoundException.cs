0f6bed4f22a8793a4aae58a2648e5d921e99d4a8
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-13 22:25:29 -0300

    Refatorando Exceptions

--- src/CashFlow.Exception/ExceptionsBase/ExpenseNotFoundException.cs
+++ src/CashFlow.Exception/ExceptionsBase/ExpenseNotFoundException.cs
@@ -1,9 +1,18 @@
-﻿namespace CashFlow.Exception.ExceptionsBase
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase
 {
     public class ExpenseNotFoundException : CashFlowException
     {
         public ExpenseNotFoundException(string message) : base(message)
         {
+        }
+
+        public override int StatusCode => (int)HttpStatusCode.NotFound;
+
+        public override List<string> GetErrors()
+        {
+            return [Message];
         }
     }
 }
