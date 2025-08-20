bba4704ed4d824aa1d20a0f8c7b5b426234d3074
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-20 10:45:27 -0300

    Iniciando endpoint para criar relatório de despesas em excel

---
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Application.UseCases.Expenses.Report.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet("excel")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetExcel(
+            [FromServices] IGetExpensesReportUseCase useCase,
+            [FromHeader] DateOnly month)
+        {
+            byte[] file = new byte[1];
+
+            if (file.Length > 0)
+                return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+
+            return NoContent();
+        }
+    }
+}
