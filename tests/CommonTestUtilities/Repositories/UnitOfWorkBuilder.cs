272c526f8cf01dbecaf49a26046fecbc51f196f7
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-26 19:44:43 -0300

    Adicionando mocks dos repositórios

---
+++ tests/CommonTestUtilities/Repositories/UnitOfWorkBuilder.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Repositories;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UnitOfWorkBuilder
+    {
+        public static IUnitOfWork Build()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+
+            return unitOfWork.Object;
+        }
+    }
+}
