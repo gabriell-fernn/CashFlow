4b3a0d15b3692ebdd993826dc7bfa13ee34125b8
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-26 19:44:16 -0300

    Criando mock para criptografia de senha

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Security.Cryptography;
+using Moq;
+
+namespace CommonTestUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        public static IPasswordEncripter Build()
+        {
+            var mock = new Mock<IPasswordEncripter>();
+
+            mock.Setup(x => x.Encrypt(It.IsAny<string>())).Returns("!@ashakm#42121_");
+
+            return mock.Object;
+        }
+    }
+}
