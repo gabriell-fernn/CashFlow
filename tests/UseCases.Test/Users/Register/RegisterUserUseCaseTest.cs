a55c59c8ea651439a2d1e428f716e0365cc1eb1c
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-27 14:44:41 -0300

    Implementando testes de nome vazio e email já existente

--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 using CommonTestUtilities.Cryptography;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
@@ -23,15 +25,50 @@ namespace UseCases.Test.Users.Register
             result.Token.Should().NotBeNullOrWhiteSpace();
         }
 
-        private RegisterUserUseCase CreateUseCase()
+        [Fact]
+        public async Task Error_Name_Empty()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var useCase = CreateUseCase();
+
+            var action = async () => await useCase.Execute(request);
+
+            var result = await action.Should().ThrowAsync<ErrorOnValidationException>();
+
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.NAME_EMTY));
+        }
+
+        [Fact]
+        public async Task Error_Email_Exists()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase(request.Email);
+
+            var action = async () => await useCase.Execute(request);
+
+            var result = await action.Should().ThrowAsync<ErrorOnValidationException>();
+
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.EMAIL_ALREADY_REGISTERED));
+        }
+
+        private RegisterUserUseCase CreateUseCase(string? email = null)
         {
             var mapper = MapperBuilder.Build();
             var unitOfWork = UnitOfWorkBuilder.Build();
             var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
             var tokenGenerator = JwtTokenGeneratorBuilder.Build();
             var passwordEncripter = PasswordEncripterBuilder.Build();
+            var readRepository = new UserReadOnlyRepositoryBuilder();
 
-            return new RegisterUserUseCase(mapper, passwordEncripter, null, writeRepository, unitOfWork, tokenGenerator);
+            if(string.IsNullOrWhiteSpace(email) == false)
+            {
+                readRepository.ExistsByEmailAsync(email);
+            }
+
+            return new RegisterUserUseCase(mapper, passwordEncripter, readRepository.Build(), writeRepository, unitOfWork, tokenGenerator);
         }
     }
 }
