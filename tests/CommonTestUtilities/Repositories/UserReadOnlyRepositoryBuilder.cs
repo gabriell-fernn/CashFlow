6cb433772b56b6037e3ecfb8880b66a3ef42e665
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-08-27 14:44:03 -0300

    Implementando mock de email já existente

---
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Domain.Repositories.Users;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UserReadOnlyRepositoryBuilder
+    {
+        private readonly Mock<IUsersReadOnlyRepository> _repository;
+
+        public UserReadOnlyRepositoryBuilder()
+        {
+            _repository = new Mock<IUsersReadOnlyRepository>();
+        }
+
+        public void ExistsByEmailAsync(string email)
+        {
+            _repository.Setup(userReadOnly => userReadOnly.ExistsByEmailAsync(email)).ReturnsAsync(true);
+        }
+
+        public IUsersReadOnlyRepository Build() => _repository.Object;
+    }
+}
