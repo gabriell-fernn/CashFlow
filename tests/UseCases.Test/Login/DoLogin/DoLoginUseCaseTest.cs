45b08b1b11c5e7c3cc5b486edfb7c790ac2b714b
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-09-01 10:46:10 -0300

    Iniciando implementação do teste de login do usuário

---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,44 @@
+﻿using CashFlow.Application.UseCases.Users.Login;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
+
+namespace UseCases.Test.Login.DoLogin
+{
+    public class DoLoginUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestLoginJsonBuilder.Build();
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Token.Should().NotBeNullOrWithSpace();
+        }
+
+        [Fact]
+        public async Task Error_User_Not_Found()
+        {
+
+        }
+
+        [Fact]
+        public async Task Error_Password_Not_Match()
+        {
+
+        }
+
+        private DoLoginUseCase CreateUseCase()
+        {
+            var passwordEncrypter = PasswordEncripterBuilder.Build();
+            var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+            var readRepository = new UserReadOnlyRepositoryBuilder().Build();
+
+            return new DoLoginUseCase(readRepository, passwordEncrypter, tokenGenerator);
+        }
+    }
+}
