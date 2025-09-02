f5708917b91fab9b3f410e82680656b696d35279
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-09-02 19:03:47 -0300

    Adicionando ambiente de Test para o banco InMemory

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using CashFlow.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -84,7 +85,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await MigrateDatabase();
+
+if (builder.Configuration.IsTestEnvironment() == false)
+{
+    await MigrateDatabase();
+}
 
 app.Run();
 
