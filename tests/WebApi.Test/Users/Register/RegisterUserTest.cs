76a944ce28a952416543b4b0d39d4e3455dca6ff
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-09-02 22:57:35 -0300

    Implementando teste em caso de erro com nome vazio

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,9 +1,12 @@
-﻿using CommonTestUtilities.Requests;
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.Users.Register
 {
@@ -12,7 +15,7 @@ namespace WebApi.Test.Users.Register
         private const string METHOD = "api/User";
         private readonly HttpClient _httpClient;
 
-        public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
+        public RegisterUserTest(CustemWebApplicationFactory webApplicationFactory)
         {
             _httpClient = webApplicationFactory.CreateClient();
         }
@@ -32,6 +35,30 @@ namespace WebApi.Test.Users.Register
 
             response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
             response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string cultureInfo)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+            var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMTY", new CultureInfo(cultureInfo));
+
+            errors.Should().HaveCount(1).And.Contain(e => e.GetString()!.Equals(expectedMessage));
         }
     }
 }
