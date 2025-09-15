c700811bf1ca461ec5c849b7e954b4587ec18a10
gabriell.fernn <gabriellfertnandess1@gmail.com>
2025-09-15 10:58:59 -0300

    Implementando Tags ao registrar e retornar uma despesa

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -15,15 +15,21 @@ namespace CashFlow.Application.AutoMapper
 
         private void ResquestToEntity()
         {
-            CreateMap<RequestExpenseJson, Expense>();
             CreateMap<RequestRegisterUserJson, User>().ForMember(dest => dest.Password, config => config.Ignore());
+
+            CreateMap<RequestExpenseJson, Expense>()
+                .ForMember(dest => dest.Tags, config => config.MapFrom(source => source.Tags.Distinct()));
+
+            CreateMap<Communication.Enums.Tag, Tag>()
+                .ForMember(dest => dest.Value, config => config.MapFrom(source => source));
         }   
 
         private void EntityToResponse()
         {
+            CreateMap<Expense, ResponseExpenseJson>()
+                .ForMember(dest => dest.Tags, config => config.MapFrom(source => source.Tags.Select(tag => tag.Value)));
             CreateMap<Expense, ResponseRegisterExpenseJson>();
             CreateMap<Expense, ResponseShortExpensesJson>();
-            CreateMap<Expense, ResponseExpenseJson>();
             CreateMap<User, ResponseUserProfileJson>();
         }
     }
--- src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
@@ -16,6 +16,11 @@ namespace CashFlow.Application.UseCases.Expenses
                 .GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
             RuleFor(expense => expense.PaymentType)
                 .IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+            RuleFor(expense => expense.Tags)
+                .ForEach(rule => 
+                { 
+                    rule.IsInEnum().WithMessage(ResourceErrorMessages.TAG_TYPE_NOT_SUPPORTED); 
+                });
         }
     }
 }
